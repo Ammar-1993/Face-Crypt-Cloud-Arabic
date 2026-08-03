5b2aa5e39287a2d7e9a6073385fb78d826ebc33d
Ammar-1993 <ammaralnggar@gmail.com>
2026-08-03 10:18:21 +0300

    chore: configure flask app to bind to all network interfaces

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = create_app()
 if __name__ == '__main__':
     debug_mode = os.environ.get('FLASK_DEBUG', 'False').lower() in ('true', '1', 't')
     port = int(os.environ.get('PORT', 8080))
-    app.run(debug=debug_mode, port=port)
+    app.run(debug=debug_mode, host='0.0.0.0', port=port)
