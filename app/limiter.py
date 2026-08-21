7e0b97633bd7175f98d5f484797cb29217f257d1
Ammar-1993 <ammaralnggar@gmail.com>
2026-08-21 07:39:11 +0300

    chore: update rate limiter storage, add gitignore rules, and silence deprecation warnings in pytest

--- app/limiter.py
+++ app/limiter.py
@@ -3,5 +3,6 @@ from flask_limiter.util import get_remote_address
 
 limiter = Limiter(
     key_func=get_remote_address,
+    storage_uri="memory://",
     default_limits=["200 per day", "50 per hour"]
 )
